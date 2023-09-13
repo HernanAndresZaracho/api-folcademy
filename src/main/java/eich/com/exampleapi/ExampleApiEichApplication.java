0e2c33fda4c5e975307957b618d09fdca60d8097
HernanAndresZaracho <hernanandreszaracho@gmail.com>
2023-09-13 20:24:13 -0300

    primer commit de la api

---
+++ src/main/java/eich/com/exampleapi/ExampleApiEichApplication.java
@@ -0,0 +1,13 @@
+package eich.com.exampleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleApiEichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApiEichApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eich/com/exampleapi/ExampleApiEichApplicationTests.java
@@ -0,0 +1,13 @@
+package eich.com.exampleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApiEichApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
