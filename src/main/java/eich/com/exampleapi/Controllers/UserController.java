0ec0ea036303d87f9dab99c8bb160e3cc9fee5a8
HernanAndresZaracho <hernanandreszaracho@gmail.com>
2023-09-23 10:28:34 -0300

    se agregaron en las clases de UserController y UserService, lo necesario para poder editar y eliminar a un usuario

--- src/main/java/eich/com/exampleapi/Controllers/UserController.java
+++ src/main/java/eich/com/exampleapi/Controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -23,8 +24,57 @@ public class UserController {
         return ResponseEntity.ok(userService.findAll());
     }
 
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserReadDTO> findUserById(@PathVariable Integer userId) {
+        UserReadDTO user = userService.findUserById(userId);
+
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
     @PostMapping
     public ResponseEntity<UserReadDTO> add(@RequestBody UserAddDTO userAddDTO){
         return ResponseEntity.ok(userService.add(userAddDTO));
     }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserReadDTO> updateUser(@PathVariable Integer userId, @RequestBody UserAddDTO updatedUserData) {
+        UserReadDTO updatedUser = userService.updateUser(userId, updatedUserData);
+
+        if (updatedUser != null) {
+            return ResponseEntity.ok(updatedUser);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<UserReadDTO> updateUser(
+            @PathVariable Integer userId,
+            @RequestBody Map<String, Object> updatedUserData
+    ) {
+        UserReadDTO updatedUser = userService.partialUpdateUser(userId, updatedUserData);
+
+        if (updatedUser != null) {
+            return ResponseEntity.ok(updatedUser);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Integer userId) {
+        boolean deleted = userService.deleteUser(userId);
+
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
--- src/main/java/eich/com/exampleapi/Services/UserService.java
+++ src/main/java/eich/com/exampleapi/Services/UserService.java
@@ -2,11 +2,14 @@ package eich.com.exampleapi.Services;
 
 import eich.com.exampleapi.Models.Domain.UserAddDTO;
 import eich.com.exampleapi.Models.Domain.UserReadDTO;
+import eich.com.exampleapi.Models.Entities.UserEntity;
 import eich.com.exampleapi.Models.Mappers.UserMapper;
 import eich.com.exampleapi.Models.Repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -26,9 +29,79 @@ public class UserService {
                 .map(userMapper::userEntityToUserReadDTO)
                 .collect(Collectors.toList());
     }
+
     public UserReadDTO add(UserAddDTO userAddDTO){
         return userMapper.userEntityToUserReadDTO(
                 userRepository.save(
                 userMapper.userAddDTOToUserEntity(userAddDTO)));
     }
+
+    public UserReadDTO findUserById(Integer userId) {
+        Optional<UserEntity> userEntityOptional = userRepository.findById(userId);
+
+        if (userEntityOptional.isPresent()) {
+            UserEntity userEntity = userEntityOptional.get();
+            return userMapper.userEntityToUserReadDTO(userEntity);
+        } else {
+            return null;
+        }
+    }
+
+    public UserReadDTO updateUser(Integer userId, UserAddDTO updatedUserData) {
+        Optional<UserEntity> userEntityOptional = userRepository.findById(userId);
+
+        if (userEntityOptional.isPresent()) {
+            UserEntity userEntity = userEntityOptional.get();
+
+            userEntity.setName(updatedUserData.getName());
+            userEntity.setSurname(updatedUserData.getSurname());
+            userEntity.setEmail(updatedUserData.getEmail());
+            userEntity.setPassword(updatedUserData.getPassword());
+
+            userRepository.save(userEntity);
+
+            return userMapper.userEntityToUserReadDTO(userEntity);
+        } else {
+            return null;
+        }
+    }
+
+    public UserReadDTO partialUpdateUser(Integer userId, Map<String, Object> updatedUserData) {
+        Optional<UserEntity> userEntityOptional = userRepository.findById(userId);
+
+        if (userEntityOptional.isPresent()) {
+            UserEntity userEntity = userEntityOptional.get();
+
+            for (Map.Entry<String, Object> entry : updatedUserData.entrySet()) {
+                String fieldName = entry.getKey();
+                Object fieldValue = entry.getValue();
+
+                if ("name".equals(fieldName)) {
+                    userEntity.setName((String) fieldValue);
+                } else if ("surname".equals(fieldName)) {
+                    userEntity.setSurname((String) fieldValue);
+                } else if ("email".equals(fieldName)) {
+                    userEntity.setEmail((String) fieldValue);
+                }
+            }
+            userRepository.save(userEntity);
+
+            return userMapper.userEntityToUserReadDTO(userEntity);
+        } else {
+            return null;
+        }
+    }
+
+
+    public boolean deleteUser(Integer userId) {
+        Optional<UserEntity> userEntityOptional = userRepository.findById(userId);
+
+        if (userEntityOptional.isPresent()) {
+            userRepository.deleteById(userId);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
